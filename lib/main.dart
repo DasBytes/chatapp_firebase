8e5d19e3db04ee1a3ec9fc54b637672d4f4b66c6
PD670 <prantadas85463@gmail.com>
2025-06-11 13:35:20 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 
 void main() async
 { 
     WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+
+    if(kIsWeb){
+
+    // run the intialization for web
+      await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: apiKey,
+    appId: appId,
+    messagingSenderId: messagingSenderId,
+    projectId: projectId));
     runApp(const MyApp());
+
+    }
+    else {
+
+         // run the intialization for web
+         await Firebase.initializeApp();
+
+
+
+    }
+
+  
 }
 
 class MyApp extends StatelessWidget {
