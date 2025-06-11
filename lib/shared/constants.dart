651785421cb62f76410cf424881ab5f48e5d0f8c
PD670 <prantadas85463@gmail.com>
2025-06-11 13:40:44 +0600

    changes

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId="1:148918255301:web:591f6b3a2d61180b507aac";
+  static String apiKey="AIzaSyDO4KtWWJpUyQkGgp41Np_Q8aBCNh8eN2Q";
+  static String messagingSenderId="148918255301";
+  static String projectId="chatappflutter-24257";
+}
