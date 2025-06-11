e33b0a63998cfc14b9d1fe4980e913f236a79706
PD670 <prantadas85463@gmail.com>
2025-06-11 14:05:34 +0600

    changes

---
+++ lib/helper/helper_function.dart
@@ -0,0 +1,18 @@
+class HelperFunctions {
+
+// keys
+     static String userLoggedInKey= "LOGGEDINKEY";
+     static String userNameKey= "USERNAMEKEY";
+     static String userEmailKey="USEREMAILKEY";
+
+
+  // saving the data to share preferences
+
+
+
+
+// getting the data from share preferences
+
+
+
+}
