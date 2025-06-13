f25f3cbd7b424bb55ccfd5927a90b37ef785fe35
PD670 <prantadas85463@gmail.com>
2025-06-13 19:53:56 +0600

    changes

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -24,8 +24,9 @@ class _LoginPageState extends State<LoginPage> {
             children:<Widget> [
                const Text("Groupie!", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
-           const SizedBox(height: 10,)
+           const SizedBox(height: 10,),
            const Text("Login now to see what they are talking!", style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),)
+          
             ],
       
           ),
