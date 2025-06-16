9dd938a221388c90fbe7011547d4869cf6e49cca
PD670 <prantadas85463@gmail.com>
2025-06-17 00:48:48 +0600

    changes

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
 class Constants {
   static String appId="1:148918255301:web:591f6b3a2d61180b507aac";
   static String apiKey="AIzaSyDO4KtWWJpUyQkGgp41Np_Q8aBCNh8eN2Q";
   static String messagingSenderId="148918255301";
   static String projectId="chatappflutter-24257";
+  final primaryColor= const Color(0xFFEe7b64);
 }
