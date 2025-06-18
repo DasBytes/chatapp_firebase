69401a24e8a43d359da6a77d4d97b2cc14b4352b
PD670 <prantadas85463@gmail.com>
2025-06-18 21:37:51 +0600

    changes

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,26 +1,24 @@
-import 'package:chatapp_firebase/pages/auth/register_page.dart';
-import 'package:chatapp_firebase/widgets/widgets.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+import 'package:chatapp_firebase/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final formKey = GlobalKey<FormState>();
-  String email = "";
-  String password = "";
-
+  String email ="";
+  String password= "";
+  String fullName= "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
+return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
@@ -34,10 +32,37 @@ class _LoginPageState extends State<LoginPage> {
                     style:
                         TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
                 const SizedBox(height: 10),
-                const Text("Login now to see what they are talking!",
+                const Text("Create your account now to chat and explore ",
                     style:
                         TextStyle(fontSize: 15, fontWeight: FontWeight.w400)),
-                Image.asset("assets/login.png"),
+                Image.asset("assets/register.png"),
+                     TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Full Name",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+
+                  // check the validator
+
+                validator: (val){
+                  if(val!.isNotEmpty){
+                    return null;
+                  }
+                  else {
+                    return "Name can not be empty";
+                  }
+                },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
                 TextFormField(
                   decoration: textInputDecoration.copyWith(
                       labelText: "Email",
@@ -93,27 +118,27 @@ class _LoginPageState extends State<LoginPage> {
 ),
 
                     child: const Text(
-                      "Sign In",
+                      "Register",
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                     onPressed: () {
-                      login();
+                      register();
                     },
                   ),
                 ),
                 const SizedBox(height: 10),
                 Text.rich(TextSpan(
-                  text: "Don't have an account? ",
+                  text: "Already have an account? ",
                   style: const TextStyle(color: Colors.black, fontSize: 14),
                   children: <TextSpan>[
                     TextSpan(
-                      text: "Register here",
+                      text: "Login now",
                       style: TextStyle(
                           color: Colors.black,
                           decoration: TextDecoration.underline),
                       recognizer: TapGestureRecognizer()
                         ..onTap = () {
-                          nextScreen(context, const RegisterPage());
+                          nextScreen(context, const LoginPage());
                         },
                     ),
                   ],
@@ -126,5 +151,9 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  login() {}
+
+  register(){
+
+  }
 }
+
