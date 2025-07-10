fb20b467831a6b820e43fe22825675e35c468d50
PD670 <prantadas85463@gmail.com>
2025-07-10 14:05:22 +0600

    changes

--- lib/pages/auth/home_page.dart
+++ lib/pages/auth/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:chatapp_firebase/helper/helper_function.dart';
+import 'package:chatapp_firebase/pages/search_page.dart';
 import 'package:chatapp_firebase/service/auth_service.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 
@@ -10,13 +13,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  String userName = "";
+  String email= "";
+
   AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    gettingUserData();
+  }
+
+  gettingUserData() async {
+    await HelperFunctions.getUserEmailFromSF().then((value) {
+      setState(() {
+        email= value! ;
+      });
+    },);
+    await HelperFunctions.getUserNameFromSF().then((val) {
+      setState(() {
+        userName = val!;
+        
+      });
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: ElevatedButton( child: Text("LOGOUT"), onPressed: () {
-       authService.signOut();
-      },)),
+      appBar: AppBar(
+        actions: [IconButton(onPressed: (){
+          nextScreen(context, const SearchPage() );
+        }, icon: const Icon(Icons.search))],
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text("Groups", style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 50),
+          children: <Widget>[
+            Icon(Icons.account_circle, 
+            size: 150,
+            color: Colors.grey[700],
+            ),
+            const SizedBox(
+              height: 15,
+
+            ),
+            Text(userName)
+
+           
+          ],
+        ),
+      ),
     );
   }
 }
