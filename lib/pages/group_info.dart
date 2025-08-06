e07b0164ca346bf476500f19462e70a26f91aed0
PD670 <prantadas85463@gmail.com>
2025-08-07 02:39:44 +0600

    updates

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -37,6 +37,10 @@ class _GroupInfoState extends State<GroupInfo> {
 
   }
 
+String getName(String r)
+{
+  return r.substring(r.indexOf("_")+1);
+}
 
   @override
   Widget build(BuildContext context) {
@@ -91,16 +95,72 @@ class _GroupInfoState extends State<GroupInfo> {
                       const SizedBox(
                         height: 5,
                       ),
-                      Text("Admin: ${widget.adminName}")
+                      Text("Admin: ${getName(widget.adminName)}")
                     ],
                   )
                 ],
               ),
-            )
+            ),
+            memberList();
           ],
         ),
       ),
 
     );
+
   }
+    memberList(){
+      return StreamBuilder(stream: members, 
+      builder: (context, AsyncSnapshot snapshot){
+        if(snapshot.hasData){
+        if(snapshot.data['members']!=null){
+          if(snapshot.data['members'].length!=0){
+            return ListView.builder(
+              itemCount: snapshot.data['members'].length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        getName(snapshot.data['members'][index])
+                        .substring(0,1)
+                        .toUpperCase(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    title: Text(getName(snapshot.data['members'][index])),
+                    subtitle: Text(),
+                  ),
+                );
+              },
+            );
+          }
+          else {
+            return const Center(
+              child: Text("No Members"),
+
+            );
+          }
+        }
+        else{
+          return const Center(
+            child: Text("No Members"),
+          );
+        }
+        }
+        else{
+          return Center(
+
+          )
+        }
+      });
+    }
 }
