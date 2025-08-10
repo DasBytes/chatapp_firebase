3c7fd01b633557540c082184de8eefdecfe60269
PD670 <prantadas85463@gmail.com>
2025-08-10 14:02:44 +0600

    changes

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -141,4 +141,15 @@ Future toggleGroupJoin(
      }
 }
 
+
+// send message
+sendMessage( String groupId, Map<String, dynamic> chatMessageData) async {
+  groupCollection.doc(groupId).collection("messages").add(chatMessageData);
+  groupCollection.doc(groupId).update({
+    "recentMessage": chatMessageData['message'],
+    "recentMessageSender": chatMessageData['sender'],
+    "recentMessageTime" : chatMessageData['time'].toString(),
+  });
+}
+
 }
