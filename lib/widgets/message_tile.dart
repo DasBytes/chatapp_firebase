823226c2f3b5ba3d62726ad70ccaba6fadd9cb75
PD670 <prantadas85463@gmail.com>
2025-08-10 14:32:11 +0600

    final

--- lib/widgets/message_tile.dart
+++ lib/widgets/message_tile.dart
@@ -22,39 +22,49 @@ class _MessageTileState extends State<MessageTile> {
   Widget build(BuildContext context) {
 
     return Container(
-      margin: widget.sentByMe
-      ? const EdgeInsets.only(left: 30)
-      : const EdgeInsets.only(right: 30),
-      padding: const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
-      decoration: BoxDecoration(
-        borderRadius: widget.sentByMe
-        ? const BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-          bottomLeft: Radius.circular(20),
-        )
-        : const BorderRadius.only(
-          topLeft: Radius.circular(20),
-          topRight: Radius.circular(20),
-          bottomRight: Radius.circular(20),
+      padding: EdgeInsets.only(
+        top: 4,
+        bottom: 4,
+        left: widget.sentByMe ? 0 : 24,
+        right: widget.sentByMe ? 24 : 0
+      ),
+      alignment: widget.sentByMe? Alignment.centerRight : Alignment.centerLeft,
+      
+      child: Container(
+        margin: widget.sentByMe
+        ? const EdgeInsets.only(left: 30)
+        : const EdgeInsets.only(right: 30),
+        padding: const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+          borderRadius: widget.sentByMe
+          ? const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+          )
+          : const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+          color: widget.sentByMe
+          ?Theme.of(context).primaryColor
+          : Colors.grey[700]
+        ),      
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.sender.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 13,fontWeight: FontWeight.bold ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(widget.message, textAlign: TextAlign.center, style: const TextStyle(fontSize: 16),)
+          ],
         ),
-        color: widget.sentByMe
-        ?Theme.of(context).primaryColor
-        : Colors.grey[700]
-      ),      
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.sender.toUpperCase(),
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 13,fontWeight: FontWeight.bold ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Text(widget.message, textAlign: TextAlign.center, style: const TextStyle(fontSize: 16),)
-        ],
       ),
     );
   }
