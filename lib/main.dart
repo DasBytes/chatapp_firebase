073732091f9f0381acf54ea6d7b3544bf4b5b9bc
PD670 <prantadas85463@gmail.com>
2025-08-12 00:44:17 +0600

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,27 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() async
-{ 
-    WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    // run the initialization for web
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: Constants.apiKey,
+        appId: Constants.appId,
+        messagingSenderId: Constants.messagingSenderId,
+        projectId: Constants.projectId,
+       
+      ),
+    );
+  } else {
+    // run the initialization for mobile
+    await Firebase.initializeApp();
+  }
 
-    if(kIsWeb){
-
-    // run the intialization for web
-      await Firebase.initializeApp(options: FirebaseOptions(
-    apiKey: Constants.apiKey,
-    appId: Constants.appId,
-    messagingSenderId: Constants.messagingSenderId,
-    projectId: Constants.projectId));
-    runApp(const MyApp());
-
-    }
-    else {
-
-         // run the intialization for web
-         await Firebase.initializeApp();
-           runApp(const MyApp()); 
-
-
-
-    }
-
-  
+  runApp(const MyApp());
 }
-
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -45,34 +36,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   bool _isSignedIn = false;
-    @override
+
+  @override
   void initState() {
     super.initState();
     getUserLoggedInStatus();
   }
 
-   getUserLoggedInStatus() async {
-    await HelperFunctions.getUserLoggedInStatus().then((value){
-    if(value!=null){
-      setState(() {
-        _isSignedIn= value;
-      });
-
-    }
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSignedIn = value;
+        });
+      }
     });
-   }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Constants().primaryColor, 
-        scaffoldBackgroundColor: Colors.white
+        primaryColor: Constants().primaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-        debugShowCheckedModeBanner: false,
-        home: _isSignedIn? const HomePage(): const LoginPage(),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage(),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 const textInputDecoration = InputDecoration(
   labelStyle: TextStyle(color: Colors.black, fontWeight: FontWeight.w300),
   focusedBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFFEe7b64), width: 2),
+    borderSide: BorderSide(color: Color(0xFFEE7B64), width: 2),
   ),
    enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFFEe7b64), width: 2),
+    borderSide: BorderSide(color: Color(0xFFEE7B64), width: 2),
   ),
    errorBorder: OutlineInputBorder(
-    borderSide: BorderSide(color: Color(0xFFEe7b64), width: 2),
+    borderSide: BorderSide(color: Color(0xFFEE7B64), width: 2),
   ),
 );
 
